57c3e82c2253473ad874a85ddf18c14bdfe3909f
Konstantin <nikitin.konstatnin@gmail.com>
2010-09-22 20:45:02 +0400

    Box method

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ from complex_box import complex_box
 from neuro import Neuron, NeuralNetwork
 
 dimensions = 2
-neurons = 7
-delta = 1000.0
+neurons = 5
+delta = 10000.0
 
 
 def f(x):
@@ -46,8 +46,17 @@ def to_minimize(l):
 
 
 l = complex_box(to_minimize, (dimensions + 2) * neurons,
-                  [-200.0, 1.0, -1.0, -1.0] * neurons,
-                  [ 200.0, 7.0,  1.0,  1.0] * neurons)
+                  [-200.0, 1.0, -3.0, -3.0] * neurons,
+                  [ 200.0, 7.0,  4.0,  4.0] * neurons)
+
+print """Solution is complete
+    %s neurons
+    Complex box method
+    f = x + y
+    [-200..200, 1..7, -3..4, -3..4]
+    40 point in area
+    40 points on border
+    delta = %s""" % (neurons, delta)
 
 for i in xrange(neurons):
     print "%s * exp( -((%s - x)^2 + (%s - y)^2)/(%s^2) ) + " % (l[4 * i], l[4 * i + 2], l[4 * i + 3], l[4 * i + 1])
