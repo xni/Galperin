09bacb1f1e34a00c5faeb823caac2bc278595830
Konstantin <nikitin.konstatnin@gmail.com>
2010-09-26 20:44:42 +0400

    Matplotlib for plotting

--- src/main_box.py
+++ src/main_box.py
@@ -3,14 +3,17 @@ import math
 from random import random
 from time import time
 
+import matplotlib.pyplot as plot
+
 from estimate import make_quad_estimate
 from complex_box import complex_box
 from neuro import Neuron, NeuralNetwork
+import neuro.draw
 
 
 dimensions = 2
 neurons = 3
-delta = 10000.0
+delta = 100.0
 
 current = []
 inner = []
@@ -37,27 +40,26 @@ def network_from_list(l):
                                 l[i * p + 2 : i * p + 2 + dimensions]))
     return N
 
-
+inner = [[random(), random()] for i in range(15)]
+border = ([[0.0, random()] for i in range(25)] + 
+          [[1.0, random()] for i in range(25)] + 
+          [[random(), 0.0] for i in range(25)] + 
+          [[random(), 1.0] for i in range(25)]) 
 
 
 def to_minimize(l):
     global current
     global inner
     global border
-    inner = [[random(), random()] for i in range(5)]
-    border = ([[0.0, random()] for i in range(25)] + 
-          [[1.0, random()] for i in range(25)] + 
-          [[random(), 0.0] for i in range(25)] + 
-          [[random(), 1.0] for i in range(25)]) 
-    current = l
     nn = network_from_list(l)
+    current = l
     return J(f, g, inner, border, nn)
 
 t_start = time()
 try:
     l = complex_box(to_minimize, neurons * (2 + dimensions), 
-                    [-250.0, 1.0, -5.0, -5.0] * neurons, 
-                    [250.0, 10.0, 6.0, 6.0] * neurons)
+                    [-250.0, 1.0, 0.0, 0.0] * neurons, 
+                    [250.0, 10.0, 4.0, 4.0] * neurons)
 except KeyboardInterrupt, e:
     l = current
 t_end = time()
@@ -83,3 +85,18 @@ for i in xrange(neurons):
     ns = 2 + dimensions
     print "(%s)*exp(-(((%s)-x)^2+((%s)-y)^2)/((%s)^2))+" % (l[ns * i], l[ns * i + 2], l[ns * i + 3], l[ns * i + 1]),
 print "0)"
+neuro.draw.draw(resulting_nn, (0.0, 0.0, 4.0, 4.0), (0.0, 0.0, 1.0, 1.0), "/tmp/cfg.png")
+
+for c_y in xrange(3):
+    y = c_y * 0.5
+    xs = [0.01*x for x in range(100)]
+    ys = [resulting_nn([x, y]) for x in xs]
+    zs = [g([x, y]) for x in xs]
+    plot.figure(c_y)
+    plot.xlabel('$x$')
+    plot.ylabel('$y$')
+    plot.title(ur'Погрешность полученного решения в сечении $y = %.1f$' % y)
+    plot.plot(xs, ys, label=ur'полученное решение', color='red')    
+    plot.plot(xs, zs, label=ur'точное решение', color='green')    
+    plot.legend(loc='upper right')
+    plot.savefig("/tmp/fig_%s.png" % c_y)
--- src/neuro/draw.py
+++ src/neuro/draw.py
@@ -2,17 +2,40 @@
 
 from PIL import Image, ImageDraw
 
-def draw(NN, box, filename):
+def draw(NN, box, area, filename):
     NN.neurons.sort(key = lambda n: -n.a)
     i = Image.new("RGB", [1000, 1000])
     d = ImageDraw.Draw(i)
-    l, t, r, b = box
-    d.rectangle((0, 0, 1000, 1000), fill=256 * 256 * 256 - 1)
+    l, b, r, t = box
+    l1, b1, r1, t1 = area
+    d.rectangle((0, 0, 1000, 1000), fill="#7f7f7f")
+    for ll in xrange(int(l), int(r+1)):
+        x_raw = 1000.0 * (ll - l) / (r - l)
+        d.line((x_raw, 0.0, x_raw, 1000.0), fill="#cccccc")
+    for ll in xrange(int(b), int(t+1)):
+        y_raw = 1000.0 * (ll - t) / (b - t)
+        d.line((0.0, y_raw, 1000.0, y_raw), fill="#cccccc")
+
     for n in NN.neurons:
-        x_s = n.center[0]
-        y_s = n.center[1]
-        x_raw = 1000.0 * (x_s - l) / (l - r)
-        y_raw = 1000.0 - 1000.0 * (y_s - t) / (b - t)
-        r = 1000.0 * n.a / (r - l)
-        d.ellipse((x_raw - r, y_raw - r, x_raw + r, y_raw + r), outline=0)
+        x_s = n.c[0]
+        y_s = n.c[1]
+        x_raw = 1000.0 * (x_s - l) / (r - l)
+        y_raw = 1000.0 * (t - y_s) / (t - b)
+        rad = 1000.0 * n.a / (r - l)
+        color = int((n.w + 250) / 500.0 * 255)
+        d.ellipse((x_raw - rad, y_raw - rad, x_raw + rad, y_raw + rad), fill=(color, color, color), outline=0)
+        
+    for n in NN.neurons:
+        x_s = n.c[0]
+        y_s = n.c[1]
+        x_raw = 1000.0 * (x_s - l) / (r - l)
+        y_raw = 1000.0 * (t - y_s) / (t - b)
+        rad = 2
+        d.ellipse((x_raw - rad, y_raw - rad, x_raw + rad, y_raw + rad), fill=0)
+        
+    x_raw = 1000.0 * (l1 - l) / (r - l)
+    y_raw = 1000.0 * (t - t1) / (t - b)
+    r_raw = 1000.0 * (r1 - l1) / (r - l)
+    d.rectangle((x_raw, y_raw, x_raw + r_raw, y_raw + r_raw), outline="#000000")
+    
     i.save(filename, "PNG")
